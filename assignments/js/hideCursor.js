c3679bef108212befac594754642504cc704a126
cyproto <kyashkarande@gmail.com>
2019-09-03 08:52:03 +0530

    Changelogs: - Add JS assignments:     1] Add Javascript validation on “Post A Job” Form as mentioned below:         * All text fields are having at-least 3 character while submitting form. It should not be allowed to submit the form if any text field is having less than 3 characters & Show error. Show error message on top of form. 	* If the user clicks on any of the Job Type options ‘Full time OR Part Time’ add a text area similar job description with the label ‘Company profile’.     2] If the user clicks on any of the Job Type options ‘Full time OR Part Time’ add a text area similar     job description with the label ‘Company profile’. - Move css stuff to a seperate directory. - Refactor a bit. - Fix indentations. - Minor bug fixes.

--- assignments/js/hideCursor.js
+++ assignments/js/hideCursor.js
@@ -6,6 +6,7 @@ function hidecursor()
     document.getElementById('hidecursor3').style.cursor='none'; 
     document.getElementById('hidecursor4').style.cursor='none'; 
     document.getElementById('hidecursor5').style.cursor='none';
+    document.getElementById('hidecursor6').style.cursor='none';
 }
 document.onmousemove = function(){
     document.body.style.cursor='default';
@@ -14,4 +15,5 @@ document.onmousemove = function(){
     document.getElementById('hidecursor3').style.cursor='default'; 
     document.getElementById('hidecursor4').style.cursor='default'; 
     document.getElementById('hidecursor5').style.cursor='default';
+    document.getElementById('hidecursor6').style.cursor='default';
 }
