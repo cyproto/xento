9aafd5fad65b31646929ee75764a7d1ab5d3f472
cyproto <kyashkarande@gmail.com>
2019-09-19 12:19:19 -0500

    Push php assignment 1. (Partial)

---
+++ assignments/phpAssigments/createDatabase.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE ykarande;
+ 
+CREATE TABLE jobs (salary int not null, job_title varchar(30) not null,	company varchar(30) not null, job_type varchar(20) not null, location varchar(20) not null, job_description text not null, company_profile text);
+ 
+INSERT INTO jobs values(300,'FrontEnd Development','FACEBOOK, INC','Partime','WESTERN CITY, UK','sadasd asdasd asdasdasd');
---
+++ assignments/phpAssigments/jsAssignments/js2_2.js
@@ -0,0 +1,21 @@
+setTimeout(function(){
+    document.getElementById("showerr").style.display='none';
+}, 5000);
+setTimeout(function(){
+    document.getElementById("showerr1").style.display='none';
+}, 5000);
+setTimeout(function(){
+    document.getElementById("submitsuccess").style.display='none';
+}, 5000);
+document.getElementById("dismiss").onclick = function()
+{
+    document.getElementById("showerr").style.display='none';
+}
+document.getElementById("dismiss1").onclick = function()
+{
+    document.getElementById("showerr1").style.display='none';
+}
+document.getElementById("dismiss2").onclick = function()
+{
+    document.getElementById("submitsuccess").style.display='none';
+}
