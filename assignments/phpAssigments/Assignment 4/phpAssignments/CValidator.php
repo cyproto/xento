63ad7dadb45d1b7eb07270dc9df87c016481f7d9
cyproto <kyashkarande@gmail.com>
2019-09-23 18:47:59 +0530

    Push php assignment 4. - Also nuke empty file in assignment 2.

---
+++ assignments/phpAssigments/Assignment 4/phpAssignments/CValidator.php
@@ -0,0 +1,32 @@
+<?php 
+    function validate($job_title,$company_name,$location,$job_description,$salary,$job_type)
+    {
+        if(empty($job_title)||strlen($job_title)<3||empty($company_name)||strlen($company_name)<3||empty($location)||strlen($location)<3||empty($job_description)||strlen($job_description)<3||empty($salary)||empty($job_type))
+        {
+            return 1;
+        }
+        else if(!preg_match("/^[a-zA-Z][a-zA-Z ]*$/",$job_title))
+        {
+            return 2;
+        }
+        else
+        {
+            require 'config/connect.php';
+            if(isset($salary[0]) and isset($salary[1]))
+            {
+                $sal = '800';
+            }
+            else if(!isset($salary[0]))
+            {
+                $sal = '300';
+            }
+            else
+            {
+                $sal = '500';
+            }
+            $insertQuery = "INSERT INTO jobs(salary,job_title,company,job_type,location,job_description) VALUES('$sal','$job_title','$company_name','$job_type','$location','$job_description')";
+            $pdo->query($insertQuery);
+            return 0;
+        }
+    }
+?>
